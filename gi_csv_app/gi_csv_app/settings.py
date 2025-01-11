ad32ff25b907cb873d8b1bcbfdddcefea6922553
YADHU KRISHNA P V <116164765+yadhu12345@users.noreply.github.com>
2025-01-11 19:38:30 +0530

    Docker file added, postgres db added

--- gi_csv_app/gi_csv_app/settings.py
+++ gi_csv_app/gi_csv_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%he#4n$5@-jc9l&h)c#nr6em15%bpf4y%@9)78kwkd33-immvq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'gi_csv_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'main_db', 
+        'USER': 'postgres', 
+        'PASSWORD': 'postgres', 
+        'HOST': '192.168.1.35', 
+        'PORT': '5432', 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
